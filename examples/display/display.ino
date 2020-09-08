f42546a1f86a979eace3c38f99c1de943f17aaaf
Anderson Costa <arcostasi@gmail.com>
2020-09-08 08:44:57 -0300

    Separate examples

---
+++ examples/display/display.ino
@@ -0,0 +1,39 @@
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include <Wire.h>
+#include "FastLED.h"
+
+// SSD1306
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+#define DELAY_DISPLAY 1000
+
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+
+unsigned int timerDisplay = 0;
+
+int counter = 0;
+
+// The setup function runs once when you press reset or power the board
+void setup() {
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3D);
+  display.display();
+  delay(1000);
+}
+
+// The loop function runs over and over again forever
+void loop() {
+  if ((millis() - timerDisplay) > DELAY_DISPLAY) {
+    timerDisplay = millis();
+    display.clearDisplay();
+    display.setTextSize(1);
+    display.setTextColor(SSD1306_WHITE);
+    display.setCursor(4, 4);
+    display.println(F("Hello, Wokwi!"));
+    display.setTextSize(2);
+    display.setCursor(54, 24);
+    display.println(counter);
+    display.display();
+    counter++;
+  }
+}

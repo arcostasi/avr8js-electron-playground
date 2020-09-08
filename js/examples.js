70470fa54b08a4f794e59b4092c549ac82c9f4aa
Anderson Costa <arcostasi@gmail.com>
2020-09-08 09:54:59 -0300

    Examples update

--- js/examples.js
+++ js/examples.js
@@ -1,38 +1,51 @@
-const exPath = './examples/';
+class Examples {
 
-blinks = function() {
-  readTextFile(exPath + 'blinks/blinks.ino');
+  path() {
+    return './examples/';
+  }
+
+  blinks() {
+    readTextFile(this.path() + 'blinks/blinks.ino');
+  }
+
+  rgb() {
+    readTextFile(this.path() + 'rgb/rgb.ino');
+  }
+
+  fire() {
+    readTextFile(this.path() + 'fire/fire.ino');
+  }
+
+  metalballs() {
+    readTextFile(this.path() + 'metaballs/metaballs.ino');
+  }
+
+  display() {
+    readTextFile(this.path() + 'display/display.ino');
+  }
+
+  ssd1306() {
+    readTextFile(this.path() + 'ssd1306/ssd1306.ino');
+  }
+
+  dht22() {
+    readTextFile(this.path() + 'dht22/dht22.ino');
+  }
+
 }
 
-rgb = function() {
-  readTextFile(exPath + 'rgb/rgb.ino');
-}
-
-metalballs = function() {
-  readTextFile(exPath + 'metaballs/metaballs.ino');
-}
-
-display = function() {
-  readTextFile(exPath + 'display/display.ino');
-}
-
-ssd1306 = function() {
-  readTextFile(exPath + 'ssd1306/ssd1306.ino');
-}
-
-dht22 = function() {
-  readTextFile(exPath + 'dht22/dht22.ino');
-}
+let examples = new Examples;
 
 document.addEventListener('DOMContentLoaded', (event) => {
   // Create tab examples
-  createTab("blinks", "blinks()");
-  createTab("rgb", "rgb()");
-  createTab("metalballs", "metalballs()");
-  createTab("display", "display()");
-  createTab("ssd1306", "ssd1306()");
-  createTab("dht22", "dht22()");
+  createTab("blinks", "examples.blinks()");
+  createTab("rgb", "examples.rgb()");
+  createTab("fire", "examples.fire()");
+  createTab("metalballs", "examples.metalballs()");
+  createTab("display", "examples.display()");
+  createTab("ssd1306", "examples.ssd1306()");
+  createTab("dht22", "examples.dht22()");
 
   // Load blink example
-  blinks();
+  examples.blinks();
 });

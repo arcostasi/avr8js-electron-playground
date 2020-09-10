590682de2c52aad42949bcb3ba2cd260e918dc9c
Anderson Costa <arcostasi@gmail.com>
2020-09-10 09:59:20 -0300

    New features

--- js/monaco-editor.js
+++ js/monaco-editor.js
@@ -1,4 +1,7 @@
 let editor;
+let projectPath;
+let projectName;
+let projectHex;
 
 require.config({ paths: { 'vs': './node_modules/monaco-editor/min/vs' } });
 require(['vs/editor/editor.main'], function () {
@@ -22,19 +25,55 @@ setModel = function(code, ext) {
   });
 }
 
-readTextFile = function(file, ext = 'cpp')
+getProjectPath = function() {
+  return projectPath;
+}
+
+setProjectPath = function(folder) {
+  projectPath = folder;
+}
+
+getProjectName = function(ext = '') {
+  return projectName.concat(ext);
+}
+
+setProjectName = function(name) {
+  projectName = name;
+}
+
+getProjectHex = function() {
+  return projectHex;
+}
+
+setProjectHex = function(folder, fileHex) {
+  projectHex = folder.concat(fileHex);
+}
+
+readTextFile = function(folder, fileName)
 {
   let rawFile = new XMLHttpRequest();
 
-  rawFile.open("GET", file, true);
+  rawFile.open("GET", folder.concat(fileName), true);
 
   rawFile.onreadystatechange = function () {
     if (rawFile.readyState === 4) {
       if (rawFile.status === 200 || rawFile.status == 0) {
-        setModel(rawFile.responseText, ext);
+        setModel(rawFile.responseText, 'cpp');
       }
     }
   }
 
   rawFile.send();
+}
+
+loader = function (path, name) {
+  // Set project path & name
+  setProjectPath(path);
+  setProjectName(name);
+
+  // Set project hex filename
+  setProjectHex(getProjectPath(), getProjectName('.hex'));
+
+  // Load project file
+  readTextFile(getProjectPath(), getProjectName('.ino'));
 }
